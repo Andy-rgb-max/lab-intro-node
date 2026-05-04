fa4afc1466f881bebf0ddad1a198e74bf171a192
Andy Perrotte <andy.perrotte@hotmail.com>
2026-05-04 07:11:48 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  get(pos) {
+  if (this.items[pos] === undefined) {
+    throw new Error("OutOfBounds");
+  }
+  return this.items[pos];
 }
+  }
+
+  max() {
+  if (this.length === 0) {
+    throw new Error("Empty SortedList");
+  }
+  return this.items[this.items.length - 1];
+}
+  
+
+  min() {
+     if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  return this.items[0];
+  }
+
+  sum() {
+      let total = 0;
+
+  for (let i = 0; i < this.items.length; i++) {
+    total = total + this.items[i];
+  }
+
+  return total;
+  }
+
+  avg() {
+     if (this.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+
+  return this.sum() / this.length;
+  }
+
 
 module.exports = SortedList;
